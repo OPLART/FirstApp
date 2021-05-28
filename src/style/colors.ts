9b4309c284884424af87ac6d3ee9dcc0ae31df8d
Miguel <miguelcorrear79@gmail.com>
2021-05-28 17:40:26 -0300

    Primeiro commit

---
+++ src/style/colors.ts
@@ -0,0 +1,18 @@
+export default {
+  body_light: '#AAB2AD',
+  background: '#FFFFFF',
+  shape: '#F0F0F0',
+  white: '#FFFFFF',
+  gray: '#CFCFCF',
+  red: '#E83F5B',
+  maximumbluegreen: '#3fc1c0ff',
+  maximumbluegreen2: '#20bac5ff',
+  pacificblue: '#00b2caff',
+  pacificblue2: '#04a6c2ff',
+  bluegreen: '#0899baff',
+  cgblue: '#0f80aaff',
+  sapphireblue: '#16679aff',
+  lapislazuli: '#1a5b92ff',
+  usafablue: '#1c558eff',
+  yaleblue: '#1d4e89ff',
+};
